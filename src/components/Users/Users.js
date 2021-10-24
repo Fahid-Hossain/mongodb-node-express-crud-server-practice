2ae7c83a4fd62c87d53da530e6e349e8c9d3fd7b
Fahid Hossain <fahidhossain2021@gmail.com>
2021-10-24 20:10:24 +0600

    handle delete from ui without reload page

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -11,7 +11,9 @@ const Users = () => {
     //delete user from ui and server
     const deleteUser = (id)=>{
         // console.log(id);
-        const url = `http://localhost:5000/users/${id}`
+        const procced = window.confirm("Are sure to delete this user?")
+        if(procced){
+            const url = `http://localhost:5000/users/${id}`
         fetch(url,{
             method:"DELETE"
         })
@@ -21,7 +23,11 @@ const Users = () => {
             if(data.deletedCount > 0){
                 alert("deleted successfully")
             }
+            //handle delete from ui without reload page
+            const remainingUsers = users.filter(user=>user._id !== id);
+            setUser(remainingUsers);
         })
+        }
 
     }
     return (
