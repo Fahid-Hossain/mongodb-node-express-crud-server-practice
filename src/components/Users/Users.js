6968861fe667e3d10919a185c344c747cec39695
Fahid Hossain <fahidhossain2021@gmail.com>
2021-10-24 20:03:36 +0600

    delete user from ui and server

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -7,11 +7,31 @@ const Users = () => {
         .then(res=>res.json())
         .then(data=>setUser(data))
     },[])
+
+    //delete user from ui and server
+    const deleteUser = (id)=>{
+        // console.log(id);
+        const url = `http://localhost:5000/users/${id}`
+        fetch(url,{
+            method:"DELETE"
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            // console.log(data);
+            if(data.deletedCount > 0){
+                alert("deleted successfully")
+            }
+        })
+
+    }
     return (
         <div>
             <h1>Users Available : {users.length}</h1>
             {
-                users.map(user=><li>{user.name} :: {user.email} :: {user.phone}</li>)
+                users.map(user=><li>{user.name} :: {user.email} :: {user.phone}
+                <button onClick={()=>deleteUser(user._id)}>X</button>
+                <button>update</button>
+                </li>)
             }
         </div>
     );
